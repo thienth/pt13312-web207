6ba4b147770f59c7fd37103dc9a5346e780598a0
thienth32 <thienth32@gmail.com>
2019-07-10 18:00:15 +0700

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,37 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pt13312-web207';
+	title = 'pt13312-web207';
+
+	user = {
+		name: 'thienth',
+		age: 30,
+		address: '15 dong quan',
+		hometown: 'nam dinh'
+	}
+
+  	heroList = [
+	  { id: 11, name: 'Dr Nice' },
+	  { id: 12, name: 'Narco' },
+	  { id: 13, name: 'Bombasto' },
+	  { id: 14, name: 'Celeritas' },
+	  { id: 15, name: 'Magneta' },
+	  { id: 16, name: 'RubberMan' },
+	  { id: 17, name: 'Dynama' },
+	  { id: 18, name: 'Dr IQ' },
+	  { id: 19, name: 'Magma' },
+	  { id: 20, name: 'Tornado' }
+	];
+
+	removeHero = (heroId) => {
+		this.heroList = this.heroList.filter(
+			(item) => item.id != heroId
+		)
+	}
+
+
+
+
+
+
 }
