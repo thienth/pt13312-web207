7882207a1b9630d97ab24dd2eac045c2cde043f4
thienth32 <thienth32@gmail.com>
2019-08-02 17:16:43 +0700

    form control

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule }    from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -20,7 +22,8 @@ import { CateAddComponent } from './screens/cate-add/cate-add.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/screens/cate-add/cate-add.component.ts
+++ src/app/screens/cate-add/cate-add.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {CategoryService} from '../../services/category.service';
 import { Router } from '@angular/router';
+import { FormControl } from '@angular/forms';
+
 @Component({
   selector: 'app-cate-add',
   templateUrl: './cate-add.component.html',
@@ -11,18 +13,23 @@ export class CateAddComponent implements OnInit {
   constructor(private cateService: CategoryService,
   				private router: Router) { }
   category = {
-  	name: "",
-  	image: ""
+  	name: new FormControl(''),
+  	image: new FormControl('')
   }
   ngOnInit() {
   }
   saveCategory(){
-  	this.cateService.addCategory(this.category)
+    let sendData = {
+      name: this.category.name.value,
+      image: this.category.image.value
+    }
+
+  	this.cateService.addCategory(sendData)
   					.subscribe(data => {
   						console.log(data);
   						this.category = {
-  							name: "",
-  							image: ""
+  							name: new FormControl(''),
+                image: new FormControl('')
   						}
   						this.router.navigate(['/']);
   					});
